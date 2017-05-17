6142cfc0aa8831682123e21dd9c6dc0fda7ee7ed
Bo Song <bo.song@dianrong.com>
2017-05-17 13:33:31 +0800

    README.md added

--- src/js/entry.js
+++ src/js/entry.js
@@ -131,13 +131,6 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -157,3 +150,10 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
